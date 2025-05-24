6e56bd4af8785d47010a2c53968e173e3ade07a5
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-24 21:23:53 +0200

    Add mobile menu

--- src/cloneugc/static/cloneugc/actor-list.js
+++ src/cloneugc/static/cloneugc/actor-list.js
@@ -49,3 +49,46 @@ const closeBtn = document.querySelector("dialog#create-actor button#close");
 closeBtn.addEventListener("click", () => {
   dialog.close();
 });
+
+{
+  let initBodyOverflow;
+  let initHtmlOverflow;
+
+  const navTrigger = document.querySelector("button#nav-trigger");
+  const nav = document.querySelector("dialog#nav");
+
+  navTrigger.addEventListener("click", () => {
+    initBodyOverflow = document.body.style.overflow;
+    initHtmlOverflow = document.documentElement.style.overflow;
+
+    document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
+
+    nav.showModal();
+  });
+
+  nav.addEventListener("click", (event) => {
+    const rect = nav.getBoundingClientRect();
+
+    const outside =
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom;
+
+    if (outside) {
+      nav.close();
+    }
+  });
+
+  nav.addEventListener("close", () => {
+    document.body.style.overflow = initBodyOverflow;
+    document.documentElement.style.overflow = initHtmlOverflow;
+  });
+
+  const closeBtn = document.querySelector("dialog#nav button#close");
+
+  closeBtn.addEventListener("click", () => {
+    nav.close();
+});
+}
