d5284504ba1db2c1221a869b7eddfc304be6ef45
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-27 05:01:55 +0200

    Add ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,54 @@ output "celery_queue_url" {
   value       = aws_sqs_queue.celery_queue.url
   description = "The URL of the created SQS queue for Celery."
 }
+
+resource "aws_security_group" "server_sg" {
+  name_prefix = "cloneugc-server-sg-"
+  description = "Allow SSH, HTTP, and HTTPS"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                         = "ami-084568db4383264d4"
+  instance_type               = "t3.medium"
+  vpc_security_group_ids      = [aws_security_group.server_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "zlpkhr@Azamats-MacBook-Pro"
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 40
+  }
+}
+
+output "server_public_ip" {
+  value       = aws_instance.server.public_ip
+  description = "The public IP of the server."
+}
