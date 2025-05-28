c0ee66a16dfb5355c71e4e9a11baebfcfc1a4b40
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 02:28:32 +0200

    Fix missing staticfiles setting

--- cloneugc/settings.py
+++ cloneugc/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
