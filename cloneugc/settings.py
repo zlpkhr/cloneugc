e41b22dc2efe458ed10aa89f39b0d23c92bad89a
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 06:20:39 +0200

    Add studio app

--- cloneugc/settings.py
+++ cloneugc/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "booth",
+    "studio",
 ]
 
 MIDDLEWARE = [
---
+++ studio/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from booth.models import Creator
+from shortid import shortid
+
+
+class UGC(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=6,
+        default=shortid,
+        editable=False,
+    )
+    creator = models.ForeignKey(Creator, null=True, on_delete=models.SET_NULL)
+    script = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.creator.name} - {self.id}"
