830354c24f14bd2bfe2518d1d6be96788482b519
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 09:33:54 +0200

    Add create clone trigger

--- booth/admin.py
+++ booth/admin.py
@@ -1,15 +1,36 @@
 from django.contrib import admin
 
 from .models import Creator
-from .tasks import convert_video_to_mp4
+from .tasks import convert_video_to_mp4, create_voice_clone
 
 
 @admin.register(Creator)
 class CreatorAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "language", "tags", "created_at", "updated_at")
-    list_filter = ("language", "created_at", "updated_at")
-    search_fields = ("id", "name", "tags")
-    readonly_fields = ("id", "video_mp4", "cartesia_voice_id", "created_at", "updated_at")
+    list_display = (
+        "id",
+        "name",
+        "language",
+        "tags",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "language",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "id",
+        "name",
+        "tags",
+    )
+    readonly_fields = (
+        "id",
+        "video_mp4",
+        "cartesia_voice_id",
+        "created_at",
+        "updated_at",
+    )
 
     def save_model(self, request, obj, form, change):
         # Determine what actions to take
@@ -29,6 +50,10 @@ class CreatorAdmin(admin.ModelAdmin):
         if should_normalize_video and obj.video:
             convert_video_to_mp4.delay(obj.id)
 
+        # Queue voice cloning if needed
+        if not change or self._needs_voice_reclone(obj):
+            create_voice_clone.delay(obj.id)
+
     def _needs_video_renormalization(self, obj):
         """Check if the source video has changed and needs re-normalization."""
         try:
@@ -43,3 +68,11 @@ class CreatorAdmin(admin.ModelAdmin):
             return Creator.objects.get(pk=obj.pk).video_mp4
         except Creator.DoesNotExist:
             return None
+
+    def _needs_voice_reclone(self, obj):
+        """Check if the language or video has changed and needs voice re-cloning."""
+        try:
+            existing = Creator.objects.get(pk=obj.pk)
+            return existing.video != obj.video or existing.language != obj.language
+        except Creator.DoesNotExist:
+            return bool(obj.video)
