243720012282739986628b4f97ae9e0150edf63d
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 07:33:54 +0200

    Add logging

--- booth/tasks.py
+++ booth/tasks.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 import tempfile
+import logging
 
 from celery import shared_task
 from django.core.files.storage import default_storage
 
 from booth.models import Creator
 from shortid import shortid
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -45,11 +48,17 @@ def convert_video_to_mp4(creator_id: str):
             stdout=subprocess.DEVNULL,
             stderr=subprocess.PIPE,
         )
+        logger.info(
+            f"FFmpeg ran successfully for creator {creator.name} ({creator.id})."
+        )
     except subprocess.CalledProcessError as err:
         if os.path.exists(output_video_path):
             os.remove(output_video_path)
 
         err_msg = err.stderr.decode("utf-8").strip() if err.stderr else "Unknown error"
+        logger.error(
+            f"FFmpeg failed for creator {creator.name} ({creator.id}): {err_msg}"
+        )
         raise Exception(f"FFmpeg failed: {err_msg}")
 
     try:
@@ -61,7 +70,14 @@ def convert_video_to_mp4(creator_id: str):
         creator.video_mp4.name = saved_path
         creator.save()
 
+        logger.info(
+            f'Saved converted video for creator {creator.name} ({creator.id}) at "{saved_path}".'
+        )
+
         return saved_path
     finally:
         if os.path.exists(output_video_path):
             os.remove(output_video_path)
+        logger.debug(
+            f"Removed temporary file {output_video_path} for creator {creator.name} ({creator.id})."
+        )
