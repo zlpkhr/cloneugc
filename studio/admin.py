f28867ff12aef2bb788bc614844b280f77c5f3bc
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 08:14:57 +0200

    Register ugc in the admin

--- studio/admin.py
+++ studio/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ugc
+
+
+@admin.register(Ugc)
+class UgcAdmin(admin.ModelAdmin):
+    list_display = ("id", "creator", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("id", "creator__name")
+    readonly_fields = ("id", "created_at", "updated_at")
+
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
