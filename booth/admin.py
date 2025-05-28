53e8e0ecd145a1d20527a18c913f95d05a01f260
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-28 05:35:50 +0200

    Add creator

--- booth/admin.py
+++ booth/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Creator
+
+admin.site.register(Creator)
--- booth/models.py
+++ booth/models.py
@@ -1,3 +1,18 @@
+from shortid import shortid
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=6,
+        default=shortid,
+        editable=False,
+    )
+    name = models.CharField(max_length=255)
+    video = models.FileField(upload_to="booth/creators/videos/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
