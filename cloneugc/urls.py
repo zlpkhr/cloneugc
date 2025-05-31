1e780f2b6520e93705db8fb88e9b7c5dcb682915
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-05-31 23:38:34 +0200

    Basic landing

--- cloneugc/urls.py
+++ cloneugc/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from cloneugc.views import home
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("studio/", include("studio.urls")),
@@ -26,4 +28,5 @@ urlpatterns = [
     path("inframedevice/", include("inframedevice.urls")),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", home, name="home"),
 ]
