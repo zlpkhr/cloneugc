6d3a12d41d1a0888704b2da714d1fd1f55956cbf
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-03 07:24:56 +0200

    Move uv and bun into root folder for vscode venv detection

--- cloneugc/cloneugc/settings.py
+++ cloneugc/cloneugc/settings.py
@@ -140,6 +140,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Password hashing
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
