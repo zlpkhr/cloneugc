bf25be1d7b3df173af493487168f86c109c35a8d
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-03 19:46:29 +0200

    Update contact id

--- cloneugc/apps/home/models.py
+++ cloneugc/apps/home/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from lib.shortid import shortid
 
 
 class Contact(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=6,
+        default=shortid,
+        editable=False,
+    )
     name = models.TextField()
     email = models.EmailField()
     app_details = models.TextField()
