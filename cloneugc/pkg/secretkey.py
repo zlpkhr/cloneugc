3e1e3121eb67465249f788309835a53e437b835a
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-03 07:42:23 +0200

    Update secretkey impl

--- cloneugc/pkg/secretkey.py
+++ cloneugc/pkg/secretkey.py
@@ -1,8 +1,16 @@
+from logging import getLogger
+
 import boto3
+
+logger = getLogger(__name__)
 
 sm_client = boto3.client("secretsmanager")
 
 
-def getsecretkey(name: str) -> str:
-    response = sm_client.get_secret_value(SecretId=name)
-    return response["SecretString"]
+def getsecretkey(name: str) -> str | None:
+    try:
+        response = sm_client.get_secret_value(SecretId=name)
+        return response["SecretString"]
+    except sm_client.exceptions.ResourceNotFoundException:
+        logger.error(f"Secret key {name} does not exist")
+        return None
