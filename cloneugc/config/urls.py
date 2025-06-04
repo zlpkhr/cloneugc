fd8f5e4d34034f140e982135c2b3287dd7d42dba
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-04 03:33:12 +0200

    Update admin branding

--- cloneugc/config/urls.py
+++ cloneugc/config/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+admin.site.site_header = "CloneUGC"
+admin.site.site_title = "CloneUGC"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("studio/", include("apps.studio.urls")),
