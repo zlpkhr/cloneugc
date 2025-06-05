2d9b935509916df312aed8b7501499df9f5141e1
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-05 04:42:32 +0200

    Update editor api

--- cloneugc/apps/studio/static/studio/js/script-editor.js
+++ cloneugc/apps/studio/static/studio/js/script-editor.js
@@ -6,16 +6,17 @@ import Text from "@tiptap/extension-text";
 import Break from "studio/script-editor/break";
 import Spell from "studio/script-editor/spell";
 
-new Editor({
-  element: document.querySelector("#script-editor"),
-  extensions: [Document, Text, Paragraph, Break, Spell, History],
-  editorProps: {
-    attributes: {
-      class:
-        "bg-white border border-black/20 rounded-md text-lg font-medium p-3 focus:outline-pink-700 content-noneditable:bg-stone-100 content-noneditable:text-stone-500",
+export function createScriptEditor(element) {
+  return new Editor({
+    element,
+    extensions: [Document, Text, Paragraph, Break, Spell, History],
+    editorProps: {
+      attributes: {
+        class:
+          "bg-white border border-black/20 rounded-md text-lg font-medium p-3 focus:outline-pink-700 content-noneditable:bg-stone-100 content-noneditable:text-stone-500",
+      },
     },
-  },
-  onUpdate: ({ editor }) => {
-    console.log(editor.getHTML());
-  },
-});
+  });
+}
+
+createScriptEditor(document.querySelector("#script-editor"));
