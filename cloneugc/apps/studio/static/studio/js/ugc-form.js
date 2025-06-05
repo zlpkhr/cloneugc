fc3e39b35de71e523defd7ee40c843ff4de3d7e0
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-05 05:36:43 +0200

    Update script editor usage

--- cloneugc/apps/studio/static/studio/js/ugc-form.js
+++ cloneugc/apps/studio/static/studio/js/ugc-form.js
@@ -1,32 +1,39 @@
 import { previewSpeech } from "studio/preview-speech";
 import { prepareScript } from "studio/prepare-script";
+import { createScriptEditor } from "studio/script-editor";
 
-{
-  const creatorInput = document.querySelector("#creator-input");
-  const creatorVideos = document.querySelectorAll("#creator-video");
+const scriptEl = document.querySelector("[name=script]");
+const scriptEditorEl = document.querySelector("#script-editor");
 
-  const firstVideo = creatorVideos.item(0);
+const editor = createScriptEditor(scriptEditorEl);
 
-  creatorInput.value = firstVideo.dataset.creatorId;
-  firstVideo.dataset.selected = "true";
+editor.on("update", ({ editor }) => {
+  scriptEl.value = editor.getText();
+});
 
-  for (const video of creatorVideos) {
-    video.addEventListener("click", () => {
-      for (const video of creatorVideos) {
-        video.dataset.selected = "false";
-      }
+const creatorInput = document.querySelector("#creator-input");
+const creatorVideos = document.querySelectorAll("#creator-video");
 
-      creatorInput.value = video.dataset.creatorId;
-      video.dataset.selected = "true";
-    });
-  }
+const firstVideo = creatorVideos.item(0);
+
+creatorInput.value = firstVideo.dataset.creatorId;
+firstVideo.dataset.selected = "true";
+
+for (const video of creatorVideos) {
+  video.addEventListener("click", () => {
+    for (const video of creatorVideos) {
+      video.dataset.selected = "false";
+    }
+
+    creatorInput.value = video.dataset.creatorId;
+    video.dataset.selected = "true";
+  });
 }
 
 {
   const prepareScriptTrigger = document.querySelector(
     "#prepare-script-trigger",
   );
-  const scriptEl = document.querySelector("#script");
 
   prepareScriptTrigger.addEventListener("click", async () => {
     scriptEl.disabled = true;
@@ -51,31 +58,25 @@ import { prepareScript } from "studio/prepare-script";
   });
 }
 
-{
-  const previewSpeechTrigger = document.querySelector(
-    "#preview-speech-trigger",
-  );
-  const scriptEl = document.querySelector("#script");
-  const creatorInput = document.querySelector("#creator-input");
+const previewSpeechTrigger = document.querySelector("#preview-speech-trigger");
 
-  previewSpeechTrigger.addEventListener("click", async () => {
-    previewSpeechTrigger.disabled = true;
+previewSpeechTrigger.addEventListener("click", async () => {
+  previewSpeechTrigger.disabled = true;
 
-    try {
-      const blob = await previewSpeech(creatorInput.value, scriptEl.value);
-      const url = URL.createObjectURL(blob);
-      const audio = new Audio(url);
+  try {
+    const blob = await previewSpeech(creatorInput.value, editor.getText());
+    const url = URL.createObjectURL(blob);
+    const audio = new Audio(url);
 
-      audio.addEventListener("ended", () => {
-        URL.revokeObjectURL(url);
-      });
+    audio.addEventListener("ended", () => {
+      URL.revokeObjectURL(url);
+    });
 
-      audio.play();
-    } catch (error) {
-      console.error("Failed to preview speech:", error);
-      alert("Failed to preview the speech. Try again or contact support.");
-    } finally {
-      previewSpeechTrigger.disabled = false;
-    }
-  });
-}
+    audio.play();
+  } catch (error) {
+    console.error("Failed to preview speech:", error);
+    alert("Failed to preview the speech. Try again or contact support.");
+  } finally {
+    previewSpeechTrigger.disabled = false;
+  }
+});
