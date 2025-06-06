7837edac4902a7bcbcd2552bfdf7613c3926e54a
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-06 09:46:08 +0200

    Test actor

--- test/models/actor_test.rb
+++ test/models/actor_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class ActorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid male actor" do
+    actor = actors(:valid_male)
+    assert actor.valid?
+  end
+
+  test "valid female actor" do
+    actor = actors(:valid_female)
+    assert actor.valid?
+  end
+
+  test "invalid gender" do
+    actor = actors(:invalid_gender)
+    assert_not actor.valid?
+  end
+
+  test "invalid ethnicity" do
+    actor = actors(:invalid_ethnicity)
+    assert_not actor.valid?
+  end
+
+  test "invalid age group" do
+    actor = actors(:invalid_age_group)
+    assert_not actor.valid?
+  end
 end
