eafd9cb48864ced511374c057fca47f4787d1bc4
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-06 11:27:02 +0200

    Fix model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ApplicationRecord
     content_type: { with: /\Avideo\/.*\z/ },
     aspect_ratio: { with: :portrait },
     duration: { between: 15.seconds..3.minutes }
-  validates :gender, inclusion: { in: GENDER }
-  validates :ethnicity, inclusion: { in: ETHNICITIES }
-  validates :age_group, inclusion: { in: AGE_GROUPS }
+  validates :gender, inclusion: { in: GENDER }, if: :gender?
+  validates :ethnicity, inclusion: { in: ETHNICITIES }, if: :ethnicity?
+  validates :age_group, inclusion: { in: AGE_GROUPS }, if: :age_group?
 end
