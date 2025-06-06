f860de04e90adf60dfbfe85746576706d208010a
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-06 09:32:05 +0200

    Fix actor model enums

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,23 +1,16 @@
 class Actor < ApplicationRecord
   has_one_attached :clip
 
-  enum gender: { male: "Male", female: "Female" }
-  enum ethnicity: {
-    asian: "Asian",
-    black: "Black",
-    white: "White",
-    hispanic: "Hispanic",
-    middle_eastern: "Middle Eastern"
-  }
-  enum age_group: {
-    young_adult: "Young Adult",
-    middle_aged: "Middle Aged",
-    senior: "Senior"
-  }
+  GENDER = [ "male", "female" ]
+  ETHNICITIES = [ "asian", "black", "white", "hispanic", "middle_eastern" ]
+  AGE_GROUPS = [ "young_adult", "middle_aged", "senior" ]
 
   validates :clip,
     attached: true,
     content_type: { with: /\Avideo\/.*\z/ },
     aspect_ratio: { with: :portrait },
     duration: { between: 15.seconds..3.minutes }
+  validates :gender, inclusion: { in: GENDER }
+  validates :ethnicity, inclusion: { in: ETHNICITIES }
+  validates :age_group, inclusion: { in: AGE_GROUPS }
 end
